95caeb53745e240b7862182fc1fe719efaed9ebf
Gustavo Santos <gustavosantos.development@gmail.com>
2021-05-13 23:59:35 -0300

    feat: add husk into project

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
     '.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1',
   },
 };
